73d92140ecf94ed30b6b94fd2521caae05bff835
Matthew Yarmolinsky <myarmolinsky3043@bths.edu>
2020-01-07 18:01:41 -0500

    Alert Reducer, Action, and Types

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,3 +1,4 @@
 import { combineReducers } from "redux";
+import alert from "./alert"; //bring in our alert reducer
 
-export default combineReducers({}); //'combineReducers()' takes in a object which will have any reducers we create
+export default combineReducers({ alert }); //'combineReducers()' takes in a object which will have any reducers we create
