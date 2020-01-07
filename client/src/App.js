68d1cb90ec9dad3f48ae173d52d106982a2dfa72
Matthew Yarmolinsky <myarmolinsky3043@bths.edu>
2020-01-07 14:04:21 -0500

    Created a Redux Store

--- client/src/App.js
+++ client/src/App.js
@@ -5,27 +5,34 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"; //import react router
+// Redux
+import { Provider } from "react-redux"; //the Provider connects Redux and React
+import store from "./store"; //bring in the store
 
 const App = () => (
-  //changed this from 'function App() {}' to an arrow function: 'const App = () => {}'
-  //for the router to work, we have to wrap everything inside it
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path="/" component={Landing} />
-      {/*instead of '<Landing />', we use this. we set 'exact path' equal to the index which is just '/' and the component we want to load is 'Landing'*/}
-      <section className="container">
-        {/*every page within the theme except for the landing page has a class of 'container' to push everything to the middle
+  //changed this from 'function App() {}' to an arrow function: 'const App =() => {}'
+  <Provider store={store}>
+    {/*we pass our store into the Provider*/}
+    {/*for the Provider to work, we have to wrap everything inside it*/}
+    <Router>
+      {/*for the router to work, we have to wrap everything inside it*/}
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        {/*instead of '<Landing />', we use this. we set 'exact path' equal to the index which is just '/' and the component we want to load is 'Landing'*/}
+        <section className="container">
+          {/*every page within the theme except for the landing page has a class of 'container' to push everything to the middle
         for the landing page, we want the image to go all the way over so it doesn't have the class of 'container'*/}
-        <Switch>
-          {/*wrap everything in a switch so we don't have issues, especially when we create our 'private route' component*/}
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-          {/*routes for 'register' and 'login'*/}
-        </Switch>
-      </section>
-    </Fragment>
-  </Router>
+          <Switch>
+            {/*wrap everything in a switch so we don't have issues, especially when we create our 'private route' component*/}
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+            {/*routes for 'register' and 'login'*/}
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
---
+++ client/src/reducers/index.js
@@ -0,0 +1,3 @@
+import { combineReducers } from "redux";
+
+export default combineReducers({}); //'combineReducers()' takes in a object which will have any reducers we create
