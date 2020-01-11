0fa3b62e8fb24e57ef683bca470d95d151acf53d
Matthew Yarmolinsky <myarmolinsky3043@bths.edu>
2020-01-11 10:44:41 -0500

    Load User & Set Auth Token

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react"; //bring in react and Fragment
+import React, { Fragment, useEffect } from "react"; //bring in react and Fragment
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
@@ -9,32 +9,46 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom"; //imp
 // Redux
 import { Provider } from "react-redux"; //the Provider connects Redux and React
 import store from "./store"; //bring in the store
+import { loadUser } from "./actions/auth";
+import setAuthToken from "./utils/setAuthToken";
 
-const App = () => (
-  //changed this from 'function App() {}' to an arrow function: 'const App =() => {}'
-  <Provider store={store}>
-    {/*we pass our store into the Provider*/}
-    {/*for the Provider to work, we have to wrap everything inside it*/}
-    <Router>
-      {/*for the router to work, we have to wrap everything inside it*/}
-      <Fragment>
-        <Navbar />
-        <Route exact path="/" component={Landing} />
-        {/*instead of '<Landing />', we use this. we set 'exact path' equal to the index which is just '/' and the component we want to load is 'Landing'*/}
-        <section className="container">
-          {/*every page within the theme except for the landing page has a class of 'container' to push everything to the middle
+if (localStorage.token) {
+  //we need to call this here too (not just in our auth action) because otherwise it only gets called once but we need to check it often
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    //useEffect takes in a function
+    store.dispatch(loadUser()); //dispatch 'loadUser()' directly on the store
+  }, []); //we only want this to run once, but useEffect runs as an infinite loop unless we add a second parameter to it. the second parameter is just empty [] brackets
+
+  return (
+    //changed this from 'function App() {}' to an arrow function: 'const App =() => {}'
+    <Provider store={store}>
+      {/*we pass our store into the Provider*/}
+      {/*for the Provider to work, we have to wrap everything inside it*/}
+      <Router>
+        {/*for the router to work, we have to wrap everything inside it*/}
+        <Fragment>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          {/*instead of '<Landing />', we use this. we set 'exact path' equal to the index which is just '/' and the component we want to load is 'Landing'*/}
+          <section className="container">
+            {/*every page within the theme except for the landing page has a class of 'container' to push everything to the middle
         for the landing page, we want the image to go all the way over so it doesn't have the class of 'container'*/}
-          <Alert />
-          <Switch>
-            {/*wrap everything in a switch so we don't have issues, especially when we create our 'private route' component*/}
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-            {/*routes for 'register' and 'login'*/}
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
-  </Provider>
-);
+            <Alert />
+            <Switch>
+              {/*wrap everything in a switch so we don't have issues, especially when we create our 'private route' component*/}
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+              {/*routes for 'register' and 'login'*/}
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,37 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import { REGISTER_SUCCESS, REGISTER_FAIL } from "./types";
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR
+} from "./types";
+import setAuthToken from "../utils/setAuthToken";
+
+// Load User
+export const loadUser = () => async dispatch => {
+  //we want to be able to take and use a token already stored in local storage if there is one
+  //so we made a new folder in 'src' called 'utils' for storing utility files and we made a file inside 'utils' called 'setAuthToken.js'
+  //the file is just gonna have a function which takes in a token
+  //if the token is there then it is gonna add it to the headers, and if not then it will delete it from the headers
+  if (localStorage.token) {
+    //check localStorage for the token
+    setAuthToken(localStorage.token);
+  }
+
+  try {
+    const res = await axios.get("/api/auth");
+
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data //payload will be the user so the user will be sent to the 'USER_LOADED' action type
+    });
+  } catch (err) {
+    dispatch({
+      type: AUTH_ERROR
+    });
+  }
+};
 
 // Register User
 export const register = ({ name, email, password }) => async dispatch => {
