981805be3f62549b970e4da5169bf205fa8cd8a9
Matthew Yarmolinsky <myarmolinsky3043@bths.edu>
2020-01-18 12:49:33 -0500

    Create Profile Action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -18,3 +18,49 @@ export const getCurrentProfile = () => async dispatch => {
     });
   }
 };
+
+// Create or update profile
+export const createProfile = (
+  formData,
+  history,
+  edit = false
+) => async dispatch => {
+  //we pass in the history object because it has a method called 'push()' that will redirect us to a clienside route
+  //the third parameter 'edit' is false by default. this way we know if we are creating a profile or updating it
+  try {
+    const config = {
+      //since we're sending data, we need to create a config object
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+
+    const res = await axios.post("/api/profile", formData, config);
+    //we are making a post request to '/api/profile' with the information in our formData object and with our config
+
+    dispatch({
+      //dispatch is what we dispatch to our reducer
+      type: GET_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert(edit ? "Profile Updated" : "Profile Created", "success")); //we want to display an alert saying whether a profile was created or updated so we check if edit it T/F
+
+    if (!edit) {
+      //if a profile was created, we want to redirect the user to the dashboard afterward
+      history.push("/dashboard"); //redirecting in an action is different from redirecting in components. we can't use '<Redirect></Redirect>'
+      //instead we have to pass in the 'history' object which has the 'push()' method, into which we pass where we want to redirect to
+    }
+  } catch (err) {
+    const errors = err.response.data.errors; //get any errors we had with our response
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, "danger"))); //display an alert for any error we may have
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status } //send the error message and status if there is an error with our request
+    });
+  }
+};
--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,8 +1,10 @@
 import React, { Fragment, useState } from "react"; //bring in 'useState' because this is a form and each input should be a piece of state, similar to login/register
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import { createProfile } from "../../actions/profile";
+import { Link, withRouter } from "react-router-dom"; //we have to import 'withRouter' in order to use the history object in our 'createProfile' method
 
-const CreateProfile = props => {
+const CreateProfile = ({ createProfile, history }) => {
   const [formData, setFormData] = useState({
     //first parameter is our default values/fields
     company: "",
@@ -40,6 +42,11 @@ const CreateProfile = props => {
     setFormData({ ...formData, [e.target.name]: e.target.value }); //we will use this onchange for all of our inputs
   //it will retain all the other form information while only changing the value of the part of the form the user is changing
 
+  const onSubmit = e => {
+    e.preventDefault();
+    createProfile(formData, history);
+  };
+
   return (
     <Fragment>
       <h1 className="large text-primary">Create Your Profile</h1>
@@ -48,7 +55,7 @@ const CreateProfile = props => {
         profile stand out
       </p>
       <small>* = required field</small>
-      <form className="form">
+      <form className="form" onSubmit={e => onSubmit(e)}>
         <div className="form-group">
           <select name="status" value={status} onChange={e => onChange(e)}>
             <option value="0">* Select Professional Status</option>
@@ -215,6 +222,8 @@ const CreateProfile = props => {
   );
 };
 
-CreateProfile.propTypes = {};
+CreateProfile.propTypes = {
+  createProfile: PropTypes.func.isRequired
+};
 
-export default connect()(CreateProfile);
+export default connect(null, { createProfile })(withRouter(CreateProfile)); //we must wrap 'CreateProfile' in 'withRouter()' in order to use the 'history' object
